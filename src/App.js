ef586055752ec1f0234be57c455d285ddb67f2f3
Soumitra Mandal <soumitramandal1999@gmail.com>
2021-05-24 10:30:53 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React, {useState,useEffect } from 'react';
 import './App.css';
 
 function App() {
+const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+useEffect(()=>{
+  setInterval(()=>{
+    setTime(new Date().toLocaleTimeString());
+  }, 1000);
+
+});
+
+
   return (
     <div className="App">
-     
+     <p id = "time">{time}</p>
       
     </div>
   );
