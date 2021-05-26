93836d78407165eae2f140172bb3c2e7eae1128b
Soumitra Mandal <soumitramandal1999@gmail.com>
2021-05-26 20:27:13 +0530

    to-do list

--- src/App.js
+++ src/App.js
@@ -4,29 +4,31 @@ import './App.css';
 function App() {
 
 const [nm, setNm] = useState('');
-const [name, setName] = useState('');
+const [list, setList] = useState([]);
 
 const handleClick = ()=>{
   if(nm!==''){
-    setName(nm);
+    setList([...list,nm]);
     setNm('');
   }
 
 } 
-
+const item = list.map((s) =>
+  <li key={s.toString()}>
+    {s}
+  </li>);
 
 const handleChange = (e)=>{
   setNm(e.target.value);
 } 
 
-// Use a conditonal rendering statement 
-// refer to docs
+
   return (
     <div className="App">
-    <h1>Welcome</h1>
+    <h1>To-Do List</h1>
     <input type="text" name="name" id = 'nm' onChange = {handleChange} value = {nm}/>
     <button onClick = {handleClick} id = 'btn'>Enter</button>
-<p>{name}</p>
+    <ol>{item}</ol>
     </div>
   );
 }
